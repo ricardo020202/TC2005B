dc45617040ee1338725c7f732399ce14a15e5eab
ricardo020202 <ricoroca2@hotmail.com>
2023-02-17 16:27:31 -0600

    Entrega lab 4

--- Lab 4/main.js
+++ Lab 4/main.js
@@ -90,9 +90,9 @@ document.getElementById("promedio").innerHTML = "<br><br>" +
     "El promedio de la fila 2 es: " + result1[2] + "<br>" +
     "El promedio de la fila 3 es: " + result1[4] + "<br>";
 
-function reverse_num(num){
+function reverse_num(num) {
     let reverse = 0;
-    while(num > 0){
+    while (num > 0) {
         reverse = reverse * 10 + num % 10;
         num = Math.floor(num / 10);
     }
@@ -103,3 +103,39 @@ let result2 = reverse_num(num);
 document.getElementById("reverse").innerHTML = "<br><br>" +
     "num: " + num + "<br>" +
     "El numero invertido es: " + result2 + "<br>";
+
+class Trapecio {
+    constructor(baseMayor, baseMenor, altura, lado1, lado2) {
+        this.baseMayor = baseMayor;
+        this.baseMenor = baseMenor;
+        this.altura = altura;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+    }
+    area() {
+        return ((this.baseMayor + this.baseMenor) * this.altura) / 2;
+    }
+    perimetro() {
+        return this.baseMayor + this.baseMenor + this.lado1 + this.lado2;
+    }
+}
+
+document.getElementById("trapecio").innerHTML = "<br><br>" +
+    "trapecio: <br>" +
+    "baseMayor: 5<br>" + "baseMenor: 3<br>" + "altura: 4<br>" + "lado1: 5<br>" + "lado2: 3<br>" +
+    "El area del trapecio es: " + new Trapecio(5, 3, 4, 5, 3).area() + "<br>" +
+    "El perimetro del trapecio es: " + new Trapecio(5, 3, 4, 5, 3).perimetro() + "<br>";
+
+    
+//Pruebas contador
+console.assert(result[0] == 2, "La cantidad de numeros negativos es incorrecta");
+console.assert(result[2] == 1, "La cantidad de numeros cero es incorrecta");
+console.assert(result[4] == 3, "La cantidad de numeros positivos es incorrecta");
+
+//Pruebas promedio
+console.assert(result1[0] == 2, "El promedio de la fila 1 es incorrecto");
+console.assert(result1[2] == 5, "El promedio de la fila 2 es incorrecto");
+console.assert(result1[4] == 8, "El promedio de la fila 3 es incorrecto");
+
+//Pruebas reverse_num
+console.assert(result2 == 987654321, "El numero invertido es incorrecto");
