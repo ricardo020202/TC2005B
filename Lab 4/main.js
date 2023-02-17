8e1525db8f411cefde56ae822c7c4e00aed4c5a8
ricardo020202 <ricoroca2@hotmail.com>
2023-02-16 20:03:32 -0600

    Agregar funciones

--- Lab 4/main.js
+++ Lab 4/main.js
@@ -34,3 +34,47 @@ function validarSuma() {
         validarSuma();
     }
 }
+
+function contador(array) {
+    let negative = 0;
+    let zeros = 0;
+    let positive = 0;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < 0) {
+            negative++;
+        }
+        if (array[i] == 0) {
+            zeros++;
+        }
+        if (array[i] > 0) {
+            positive++;
+        }
+    }
+    return negative + " " + zeros + " " + positive;
+}
+let array = [-4, 3, -9, 0, 4, 1];
+let result = contador(array);
+document.getElementById("contador").innerHTML = "<br><br>" +
+    "array: " + array + "<br>" +
+    "La cantidad de numeros negativos es: " + result[0] + "<br>" +
+    "La cantidad de numeros cero es: " + result[2] + "<br>" +
+    "La cantidad de numeros positivos es: " + result[4] + "<br>";
+
+function promedio(matrix) {
+    //promedio de las filas de la matriz
+    let sum = 0;
+    let count = 0;
+    for (let i = 0; i < matrix.length; i++) {
+        for (let j = 0; j < matrix[i].length; j++) {
+            sum += matrix[i][j];
+            count++;
+        }
+    }
+    return sum / count;
+}
+
+let matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]];
+let result1 = promedio(matrix);
+document.getElementById("promedio").innerHTML = "<br><br>" +
+    "matrix: " + matrix + "<br>" +
+    "El promedio de los numeros es: " + result1 + "<br>";
