f9a70ba8bed932ed5d34a72519fb9996b2a330d1
rico020202 <ricoroca2@gmail.com>
2023-02-20 21:24:08 -0600

    agregar estilos

--- Lab 6/main.js
+++ Lab 6/main.js
@@ -5,8 +5,25 @@ function validatePassword() {
     if (password != validatePassword) {
         message.innerHTML = "Passwords dont match";
         return false;
-    }else{
+    } else if (password == validatePassword && password != "" && validatePassword != "") {
         message.innerHTML = "Passwords match";
         return true;
     }
+}
+
+let font = document.getElementById("font");
+font.onmousewheel = () => {
+    const colores = [
+        "text-primary",
+        "text-secondary",
+        "text-success",
+        "text-danger",
+        "text-warning",
+        "text-info",
+        "text-light",
+        "text-dark"
+    ]
+    const index = Math.floor(Math.random() * colores.length);
+    font.classList = ["d-flex justify-content-center align-items-center"];
+    font.classList.add(colores[index]);
 }
