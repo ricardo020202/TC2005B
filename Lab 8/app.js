060abb181ffa6b4eb99aab5d472cd0164a77c99a
ricardo020202 <ricoroca2@hotmail.com>
2023-02-23 12:59:54 -0600

    agregar servidor

--- Lab 8/app.js
+++ Lab 8/app.js
@@ -1,6 +1,19 @@
-console.log("Hola mundo")
+// console.log("Hola mundo")
 
-// Manejar archivos
-const filesystem = require('fs');
+// // Manejar archivos
+// const filesystem = require('fs');
 
-filesystem.writeFileSync('hola.txt', 'hola desde node');
+// filesystem.writeFileSync('hola.txt', 'hola desde node');
+
+// setTimeout(() => console.log("check mate"), 15000);
+
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type', 'text/html');
+    response.write("Hola desde node");
+    response.end();
+});
+
+server.listen(3000);
