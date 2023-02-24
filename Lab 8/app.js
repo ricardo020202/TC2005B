7c4044b741ed02f1a8983ce4ae25ffc15077f4d4
ricardo020202 <ricoroca2@hotmail.com>
2023-02-23 21:25:08 -0600

    Agregar ejercicios

--- Lab 8/app.js
+++ Lab 8/app.js
@@ -1,8 +1,32 @@
-
 const filesystem = require('fs');
+
+// Arreglo de numeros
+let arreglo = [1,2,3,4,5,6,7,8,9,10];
+function promedioArreglo(arreglo){
+    let elementos = 0, suma = 0, promedio;
+    for (let i = 0; i < arreglo.length; i++){
+        suma += arreglo[i];
+        elementos++;
+    }
+    promedio = suma/elementos;
+
+    return promedio;
+}
+console.log(promedioArreglo(arreglo));
+
+// Recibir archivo string y escrbirlo en un .txt
+let texto = "Hola mundo";
+function escritura(texto){
+    filesystem.writeFileSync("output.txt", texto);
+}
+escritura(texto);
+
+// Problema propio
+
 
 const http = require('http');
 
+// link a proyecto anterior
 filesystem.readFile("../Lab 1/index.html",  function (err, html) {
     if (err) {
         throw err; 
