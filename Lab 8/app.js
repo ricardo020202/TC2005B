b906ef6f7ff7dc91573369c267e674acd93c0364
ricardo020202 <ricoroca2@hotmail.com>
2023-02-23 21:37:12 -0600

    Entrega Lab 8

--- Lab 8/app.js
+++ Lab 8/app.js
@@ -21,8 +21,22 @@ function escritura(texto){
 }
 escritura(texto);
 
-// Problema propio
-
+// Bubble sort
+let arreglo2 = [10,9,8,7,6,5,4,3,2,1];
+function bubbleSort(arreglo){
+    let aux;
+    for (let i = 0; i < arreglo.length; i++){
+        for (let j = 0; j < arreglo.length; j++){
+            if (arreglo[j] > arreglo[j+1]){
+                aux = arreglo[j];
+                arreglo[j] = arreglo[j+1];
+                arreglo[j+1] = aux;
+            }
+        }
+    }
+    return arreglo;
+}
+console.log(bubbleSort(arreglo2));
 
 const http = require('http');
 
