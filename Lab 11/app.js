c917679b7eb8181edbf3e610cf42d65181bc8059
ricardo020202 <ricoroca2@hotmail.com>
2023-02-28 12:38:15 -0600

    Probar middleware

--- Lab 11/app.js
+++ Lab 11/app.js
@@ -1,2 +1,15 @@
-console.log("Hola desde nodemon");
-console.log("Hola desde nodemon");
+const express = require('express');
+const app = express();
+
+//Middleware
+app.use((request, response, next) => {
+    console.log('Middleware!');
+    next(); //Le permite a la petición avanzar hacia el siguiente middleware
+});
+
+app.use((request, response, next) => {
+    console.log('Otro middleware!');
+    response.send('¡Hola mundo!'); //Manda la respuesta
+    next()
+});
+app.listen(3000);
