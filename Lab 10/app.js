a079c25c81cd109ae672dca86cb48075e562e741
rico020202 <ricoroca2@gmail.com>
2023-03-01 13:31:22 -0600

    Entrega Lab 10

--- Lab 10/app.js
+++ Lab 10/app.js
@@ -7,7 +7,8 @@ const server = http.createServer((request, response) => {
     console.log(request.url);
     if (request.url === '/') {
         response.setHeader('Content-Type', 'text/html');
-        let html = `<!DOCTYPE html>
+        let html = `
+        <!DOCTYPE html>
         <html lang="en">
         
         <head>
@@ -30,11 +31,14 @@ const server = http.createServer((request, response) => {
         <body class="grey darken-4 white-text">
             <header>
                 <nav class="#9e9e9e grey">
-                    <ul>
-                        <li><a href="/">Inicio</a></li>
-                        <li><a href="/preguntas">Preguntas</a></li>
-                        <li><a href="/ordenar">Ordenar</a></li>
-                    </ul>
+                    <div class="nav-wrapper">
+                        <a href="/" class="brand-logo">M1000r</a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="/">Inicio</a></li>
+                            <li><a href="/preguntas">Preguntas</a></li>
+                            <li><a href="/ordenar">Ordenar</a></li>
+                        </ul>
+                    </div>
                 </nav>
                 <h2 style="display: flex; justify-content: center; font-size: 50px;">BMW M1000R 2023</h2>
             </header>
@@ -238,7 +242,8 @@ const server = http.createServer((request, response) => {
         response.end();
     } else if (request.url === '/preguntas') {
         response.setHeader('Content-Type', 'text/html');
-        let preguntas = `<!DOCTYPE html>
+        let preguntas = `
+        <!DOCTYPE html>
         <html lang="en">
         <head>
             <meta charset="UTF-8">
@@ -253,16 +258,19 @@ const server = http.createServer((request, response) => {
         <body class="grey darken-4 white-text">
             <header>
                 <nav class="#9e9e9e grey">
-                    <ul>
-                        <li><a href="/">Inicio</a></li>
-                        <li><a href="/preguntas">Preguntas</a></li>
-                        <li><a href="/ordenar">Ordenar</a></li>
-                    </ul>
+                    <div class="nav-wrapper">
+                        <a href="/" class="brand-logo">M1000r</a>
+                        <ul id="nav-mobile" class="right hide-on-med-and-down">
+                            <li><a href="/">Inicio</a></li>
+                            <li><a href="/preguntas">Preguntas</a></li>
+                            <li><a href="/ordenar">Ordenar</a></li>
+                        </ul>
+                    </div>
                 </nav>
                 <h2 style="display: flex; justify-content: center; font-size: 50px;">Preguntas</h2>
             </header>
         
-            <div id="content">
+            <div id="content" class"container">
                 <ul>
                     <li>¿Cuál es la diferencia entre Internet y la World Wide Web?
                         <ul>
@@ -398,11 +406,244 @@ const server = http.createServer((request, response) => {
         </html>`;
         response.write(preguntas);
         response.end();
-    } else if (request.url === '/ordenar') {
+    } else if (request.url === '/ordenar' && request.method === 'GET') {
         response.setHeader('Content-Type', 'text/html');
-        response.write("Reserva ahora");
+        let ordenar = `
+        <!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title>Ordenar</title>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+
+            <style>
+                body {
+                    display: flex;
+                    min-height: 100vh;
+                    flex-direction: column;
+                }
+                        
+                main {
+                    flex: 1 0 auto;
+                }
+            </style>
+        </head>
+
+        <body class="grey darken-4 white-text">
+            <nav class="#9e9e9e grey">
+                <div class="nav-wrapper">
+                    <a href="/" class="brand-logo">M1000r</a>
+                    <ul id="nav-mobile" class="right hide-on-med-and-down">
+                        <li><a href="/">Inicio</a></li>
+                        <li><a href="/preguntas">Preguntas</a></li>
+                        <li><a href="/ordenar">Ordenar</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <main>
+                <div class="container">
+                    <h1>Ordenar</h1>
+                    <form action="/ordenar" method="POST">
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="first_name" type="text" class="validate" name="nombre">
+                                <label for="first_name">Nombre</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="last_name" type="text" class="validate" name="apellido">
+                                <label for="last_name">Apellido</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="email" type="email" class="validate" name="email">
+                                <label for="email">Email</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="password" type="password" class="validate" name="password">
+                                <label for="password">Password</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="direccion" type="text" class="validate" name="direccion">
+                                <label for="direccion">Direccion</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="telefono" type="text" class="validate" name="telefono">
+                                <label for="telefono">Telefono</label>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s6">
+                                <input id="tarjeta" type="text" class="validate" name="tarjeta">
+                                <label for="tarjeta">Tarjeta</label>
+                            </div>
+                            <div class="input-field col s6">
+                                <input id="cvv" type="text" class="validate" name="cvv">
+                                <label for="cvv">CVV</label>
+                            </div>
+                        </div>
+                        </div>
+                        <button class="btn waves-effect waves-light" type="submit" name="action">Reservar
+                            <i class="material-icons right">send</i>
+                        </button>
+                    </form>
+                    <br><br>
+                </div>
+            </main>
+            
+            <footer class="page-footer #9e9e9e grey">
+                <div class="container">
+                    <div class="row">
+                        <div class="col l5 s12">
+                            <h5 class="white-text">M1000r</h5>
+                            <p class="grey-text text-lighten-4">Especificaciones tecnicas</p>
+                        </div>
+                        <div class="col">
+                            <h5 class="white-text">Navegacion</h5>
+                            <ul>
+                                <li><a class="grey-text text-lighten-3" href="/">Inicio</a></li>
+                                <li><a class="grey-text text-lighten-3" href="/preguntas">Preguntas</a></li>
+                                <li><a class="grey-text text-lighten-3" href="/ordenar">Ordenar</a></li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h5 class="white-text">Editor</h5>
+                            <ul>
+                                <li><a class="grey-text text-lighten-3" href="https://code.visualstudio.com/">Visual Studio Code</a></li>
+                            </ul>
+                        </div>
+                        <div class="col">
+                            <h5 class="white-text">Contacto</h5>
+                            <ul>
+                                <li>José Ricardo Rosales Castañeda</li>
+                                <li>A01709449</li>
+                                <li><a class="grey-text text-lighten-3" href="mailto:A01709449@tec.mx">A01709449@tec.mx</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <div class="container">
+                        © 2014 Copyright Text
+                        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                    </div>
+                </div>
+            </footer>
+        </body>
+        </html>`;
+                        
+        response.write(ordenar);
         response.end();
-    } else {
+    } 
+    else if(request.url === '/ordenar' && request.method === 'POST') {
+        let body = [];
+        request.on('data', (chunk) => {
+            body.push(chunk);
+        }).on('end', () => {
+            body = Buffer.concat(body).toString();
+            let bodySplit = body.split('&');
+            let nombre = bodySplit[0].split('=')[1];
+            let apellido = bodySplit[1].split('=')[1];
+            let email = bodySplit[2].split('=')[1];
+            let password = bodySplit[3].split('=')[1];
+            let direccion = bodySplit[4].split('=')[1];
+            let telefono = bodySplit[5].split('=')[1];
+            let tarjeta = bodySplit[6].split('=')[1];
+            let cvv = bodySplit[7].split('=')[1];
+            let orden = `Nombre: ${nombre} ${apellido} \nEmail: ${email} \nPassword: ${password} \nDireccion: ${direccion} \nTelefono: ${telefono} \nTarjeta: ${tarjeta} \nCVV: ${cvv}`;
+            filesystem.writeFileSync('orden.txt', orden);
+            response.setHeader('Content-Type', 'text/html');
+            let order = `
+                <html>
+                <head>
+                    <title>M1000r</title>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+                    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+                    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+                    <style>
+                        body {
+                            display: flex;
+                            min-height: 100vh;
+                            flex-direction: column;
+                        }
+                        
+                        main {
+                            flex: 1 0 auto;
+                        }
+                    </style>
+                </head>
+                <body class="grey darken-4 white-text">
+                    <nav>
+                        <div class="nav-wrapper #9e9e9e grey">
+                            <a href="/" class="brand-logo">M1000r</a>
+                            <ul id="nav-mobile" class="right hide-on-med-and-down">
+                                <li><a href="/">Inicio</a></li>
+                                <li><a href="/preguntas">Preguntas</a></li>
+                                <li><a href="/ordenar">Ordenar</a></li>
+                            </ul>
+                        </div>
+                    </nav>
+                    <main>
+                        <div class="container">
+                            <h1>Orden realizada</h1>
+                            <p>Gracias por su compra</p>
+                        </div>
+                    </main>
+                    <footer class="page-footer #9e9e9e grey">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col l5 s12">
+                                <h5 class="white-text">M1000r</h5>
+                                <p class="grey-text text-lighten-4">Especificaciones tecnicas</p>
+                            </div>
+                            <div class="col">
+                                <h5 class="white-text">Navegacion</h5>
+                                <ul>
+                                    <li><a class="grey-text text-lighten-3" href="/">Inicio</a></li>
+                                    <li><a class="grey-text text-lighten-3" href="/preguntas">Preguntas</a></li>
+                                    <li><a class="grey-text text-lighten-3" href="/ordenar">Ordenar</a></li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <h5 class="white-text">Editor</h5>
+                                <ul>
+                                    <li><a class="grey-text text-lighten-3" href="https://code.visualstudio.com/">Visual Studio Code</a></li>
+                                </ul>
+                            </div>
+                            <div class="col">
+                                <h5 class="white-text">Contacto</h5>
+                                <ul>
+                                    <li>José Ricardo Rosales Castañeda</li>
+                                    <li>A01709449</li>
+                                    <li><a class="grey-text text-lighten-3" href="mailto:A01709449@tec.mx">A01709449@tec.mx</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="footer-copyright">
+                        <div class="container">
+                            © 2014 Copyright Text
+                            <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+                        </div>
+                    </div>
+                </footer>
+            </body>
+            </html>`;
+            response.write(order);
+            response.end();
+        });
+    }
+    else {
         response.statusCode = 404;
         response.setHeader('Content-Type', 'text/html');
         response.write("<h1>Error 404 - Page not found</h1>");
