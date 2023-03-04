7819512f11e161c2aa138a0c8dc44838c90005fe
rico020202 <ricoroca2@gmail.com>
2023-03-04 14:21:59 -0600

    Agregar ejs y partials

--- Lab 12/routes/lab12.routes.js
+++ Lab 12/routes/lab12.routes.js
@@ -3,11 +3,35 @@ const path = require('path');
 const router = express.Router();
 
 router.get('/', (request, response, next) => {
-    response.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+    response.render('index', {
+        titulo: 'M1000R Ficha tecnica',
+    });
 });
 
 router.get('/preguntas', (request, response, next) => {
-    response.sendFile(path.join(__dirname, '..', 'views', 'preguntas.html'));
+    response.render('preguntas', {
+        titulo: 'Preguntas',
+    });
+});
+
+router.get('/ordenar', (request, response, next) => {
+    response.render('ordenar', {
+        titulo: 'Ordenar',
+    });
+});
+
+router.post('/ordenar', (request, response, next) => {
+    response.render('confirmar', {
+        titulo: 'confirmar orden',
+        nombre: request.body.nombre,
+        apellido: request.body.apellido,
+        direccion: request.body.direccion,
+        telefono: request.body.telefono,
+        email: request.body.email,
+        password: request.body.password,
+        tarjeta: request.body.tarjeta,
+        cvv: request.body.cvv,
+    });
 });
 
 module.exports = router;
