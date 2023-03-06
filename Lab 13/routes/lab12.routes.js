3d97efa039e031b55d1c30da2e4793e50bccb011
ricardo020202 <ricoroca2@hotmail.com>
2023-03-06 14:56:44 -0600

    Agregar lab 13

---
+++ Lab 13/routes/lab12.routes.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+router.get('/', (request, response, next) => {
+    response.render('index', {
+        titulo: 'M1000R Ficha tecnica',
+    });
+});
+
+router.get('/preguntas', (request, response, next) => {
+    response.render('preguntas', {
+        titulo: 'Preguntas',
+    });
+});
+
+router.get('/ordenar', (request, response, next) => {
+    response.render('ordenar', {
+        titulo: 'Ordenar',
+    });
+});
+
+router.post('/ordenar', (request, response, next) => {
+    response.render('confirmar', {
+        titulo: 'Confirmacion de Orden',
+        nombre: request.body.nombre,
+        apellido: request.body.apellido,
+        direccion: request.body.direccion,
+        telefono: request.body.telefono,
+        email: request.body.email,
+        password: request.body.password,
+        tarjeta: request.body.tarjeta,
+        cvv: request.body.cvv,
+    });
+});
+
+module.exports = router;
