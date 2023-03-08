941ae684c606fc651cab6d4cc1659299fea49837
rico020202 <ricoroca2@gmail.com>
2023-03-07 20:34:20 -0600

    Agregar express session

--- Lab 14/app.js
+++ Lab 14/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const session = require('express-session');
 
 const app = express();
 
